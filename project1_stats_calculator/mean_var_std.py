a9186df7aaa0f30bbf2541d581d26ec752248104
Stew Fraser <stewart.fraser@beatzero.co.uk>
2024-09-25 11:17:38 +0100

    more verbose calculations

--- project1_stats_calculator/mean_var_std.py
+++ project1_stats_calculator/mean_var_std.py
@@ -9,38 +9,60 @@ def calculate(list_of_numbers):
     if len(list_of_numbers) != 9:
         raise ValueError("List must contain nine numbers.")
 
+    print("\n\n\n---------- calculating various statistical measures of a 3x3 matrix...")
+
     # convert list into 3x3 numpy array
     numpy_array_3x3 = np.array(list_of_numbers).reshape(3, 3)
+    print("\n---------- numpy_array_3x3:")
+    print(numpy_array_3x3)
 
     # calc mean along both axes and flattened matrix
     mean_along_columns = np.mean(numpy_array_3x3, axis=0).tolist()
     mean_along_rows = np.mean(numpy_array_3x3, axis=1).tolist() 
     mean_of_all_elements = np.mean(numpy_array_3x3).tolist()
+    print(f"\n---------- mean_along_columns: {mean_along_columns}")
+    print(f"\n---------- mean_along_rows: {mean_along_rows}")
+    print(f"\n---------- mean_of_all_elements: {mean_of_all_elements}")
 
     # calc variance along both axes and flattened matrix
     variance_along_columns = np.var(numpy_array_3x3, axis=0).tolist()
     variance_along_rows = np.var(numpy_array_3x3, axis=1).tolist()
     variance_of_all_elements = np.var(numpy_array_3x3).tolist()
+    print(f"\n---------- variance_along_columns: {variance_along_columns}")
+    print(f"\n---------- variance_along_rows: {variance_along_rows}")
+    print(f"\n---------- variance_of_all_elements: {variance_of_all_elements}")
 
     # calc standard deviation along both axes and flattened matrix
     std_dev_along_columns = np.std(numpy_array_3x3, axis=0).tolist()
     std_dev_along_rows = np.std(numpy_array_3x3, axis=1).tolist()
     std_dev_of_all_elements = np.std(numpy_array_3x3).tolist()
+    print(f"\n---------- std_dev_along_columns: {std_dev_along_columns}")
+    print(f"\n---------- std_dev_along_rows: {std_dev_along_rows}")
+    print(f"\n---------- std_dev_of_all_elements: {std_dev_of_all_elements}")
 
     # find max along both axes and flattened matrix
     max_along_columns = np.max(numpy_array_3x3, axis=0).tolist()
     max_along_rows = np.max(numpy_array_3x3, axis=1).tolist()
     max_of_all_elements = np.max(numpy_array_3x3).tolist()
+    print(f"\n---------- max_along_columns: {max_along_columns}")
+    print(f"\n---------- max_along_rows: {max_along_rows}")
+    print(f"\n---------- max_of_all_elements: {max_of_all_elements}")
 
     # find min along both axes and flattened matrix
     min_along_columns = np.min(numpy_array_3x3, axis=0).tolist()
     min_along_rows = np.min(numpy_array_3x3, axis=1).tolist()
     min_of_all_elements = np.min(numpy_array_3x3).tolist()
+    print(f"\n---------- min_along_columns: {min_along_columns}")
+    print(f"\n---------- min_along_rows: {min_along_rows}")
+    print(f"\n---------- min_of_all_elements: {min_of_all_elements}")
 
     # calc sum along both axes and flattened matrix
     sum_along_columns = np.sum(numpy_array_3x3, axis=0).tolist()
     sum_along_rows = np.sum(numpy_array_3x3, axis=1).tolist()
     sum_of_all_elements = np.sum(numpy_array_3x3).tolist()
+    print(f"\n---------- sum_along_columns: {sum_along_columns}")
+    print(f"\n---------- sum_along_rows: {sum_along_rows}")
+    print(f"\n---------- sum_of_all_elements: {sum_of_all_elements}")
 
     # create dictionary
     calculations = {
@@ -52,5 +74,7 @@ def calculate(list_of_numbers):
         'sum': [sum_along_columns, sum_along_rows, sum_of_all_elements]
     }
 
+    print("\n---------- calculations:")
     print(calculations)
+
     return calculations
